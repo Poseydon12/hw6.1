2f896e32589bc6910f92c1b11a8b46f55b3a2632
arthur :) <arturkorol3008@gmail.com>
2025-01-08 15:40:00 +0200

    done homework

---
+++ js/script.js
@@ -0,0 +1,41 @@
+console.log("Числа від 1 до 10:");
+let i = 1;
+while (i <= 10) {
+}
+
+console.log("Парні числа від 2 до 20:");
+for (let j = 2; j <= 20; j++) {
+}
+
+console.log("Таблиця множення числа 7:");
+for (let k = 1; k <= 10; k++) {
+}
+
+console.log("Числа від 1 до 5:");
+let m = 1;
+while (m <= 5) {
+}
+
+console.log("Числа від 1 до 10, крім 7:");
+for (let n = 1; n <= 10; n++) {
+  if (n === 7) {
+    break; 
+  }
+}
+
+console.log("Числа менші за n (n = 10):");
+const n = 10;
+let p = 0;
+while (true) {
+  if (p >= n) {
+    break;
+  }
+}
+
+console.log("Числа від 1 до 20, крім кратних 3:");
+let q = 1;
+while (q <= 20) {
+  if (q % 3 === 0) {
+  }
+}
+
