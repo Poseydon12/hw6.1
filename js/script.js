4d2e5fc255e74208d4bae010da6ee54a02e903d6
arthur :) <arturkorol3008@gmail.com>
2025-01-20 15:19:22 +0200

    fixed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,28 @@
 console.log("Числа від 1 до 10:");
 let i = 1;
 while (i <= 10) {
+  console.log(i);
 }
 
 console.log("Парні числа від 2 до 20:");
 for (let j = 2; j <= 20; j++) {
+  if (j % 2 === 0)
 }
 
 console.log("Таблиця множення числа 7:");
 for (let k = 1; k <= 10; k++) {
+
 }
 
 console.log("Числа від 1 до 5:");
-let m = 1;
+let m = 1; 
 while (m <= 5) {
 }
 
 console.log("Числа від 1 до 10, крім 7:");
 for (let n = 1; n <= 10; n++) {
   if (n === 7) {
-    break; 
+    break;
   }
 }
 
@@ -33,9 +36,11 @@ while (true) {
 }
 
 console.log("Числа від 1 до 20, крім кратних 3:");
-let q = 1;
+let q = 1; 
 while (q <= 20) {
-  if (q % 3 === 0) {
-  }
+    if (q % 3 === 0) {
+        q++; 
+        continue; 
+    }
+    q++; 
 }
-
